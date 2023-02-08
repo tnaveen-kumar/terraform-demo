617f3b944dfd820cfaa5fe339f1bea12e265ffe4
tnaveen-kumar <123799811+tnaveen-kumar@users.noreply.github.com>
2023-02-08 19:44:29 +0530

    Update main.tf
    Removed the commented code

--- main.tf
+++ main.tf
@@ -14,14 +14,3 @@ resource "aws_instance" "tech_with_geeks" {
 output "PublicIP" {
   value = aws_instance.tech_with_geeks.public_ip
 }
-
-##provider "aws" "sqlserveriac" {
-##  profile    = "default"
-##  region     = "ap-south-1"
-##}
-
-##resource "aws_instance" "tech_with_geeks" {
-##  ami           = "ami-02e006a5fa21624b6"
-##  instance_type = "t2.medium"
-  
-##}
