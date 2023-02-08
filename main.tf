49d96044b52d804e00cacd1ba4d4741d8e8c668f
tnaveen-kumar <123799811+tnaveen-kumar@users.noreply.github.com>
2023-02-08 19:42:58 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,10 +1,27 @@
-provider "aws" "sqlserveriac" {
+provider "aws" {
   profile    = "default"
   region     = "ap-south-1"
 }
 
 resource "aws_instance" "tech_with_geeks" {
-  ami           = var.sqlserverdev2022
-  instance_type = var.Medium_instance_type
-  
+  ami           = var.ami
+  instance_type = var.Micro_instance_type
+  tags = {
+    name:"SQLServer_IAC"
+  }
 }
+
+output "PublicIP" {
+  value = aws_instance.tech_with_geeks.public_ip
+}
+
+##provider "aws" "sqlserveriac" {
+##  profile    = "default"
+##  region     = "ap-south-1"
+##}
+
+##resource "aws_instance" "tech_with_geeks" {
+##  ami           = "ami-02e006a5fa21624b6"
+##  instance_type = "t2.medium"
+  
+##}
---
+++ terraform.tf
@@ -0,0 +1,7 @@
+variable "ami" {
+	default = "ami-02e006a5fa21624b6"
+}
+
+variable "Micro_instance_type" {
+	default = "t2.medium"
+}
